055fc6813b7cf15a5df61fea3afe170582098aa8
Soroush <soroush.bassam@gmail.com>
2022-10-14 21:40:16 -0400

    added logo

--- nub_ai_meeting_tools/app.py
+++ nub_ai_meeting_tools/app.py
@@ -1,3 +1,5 @@
+import time
+from pyvis.network import json
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit.elements import progress
@@ -8,6 +10,8 @@ import io
 from utils import get_generated_text, extract_graph_elements
 import copy
 import nltk
+st.set_page_config(page_title='Meeting Graph', layout='centered')
+st.image('nub_ai_meeting_tools/full-stack-logo.png')
 nltk.download('punkt')
 st.title("Meeting Knowledge Graph Generator")
 
@@ -43,13 +47,13 @@ if input_file:
 
     for i, sent in enumerate(sent_list):
         with data_display:
-            st.text(sent)
-            st.text(graph_element_list[i])
+            st.text(f'Sentence {i}: {sent}')
             col1, col2 = st.columns(2)
             with col1:
-                st.text(f'Name: {graph_element_list[i]["src"][0]}')
-                st.text(f'Task: {graph_element_list[i]["dest"][0]}')
-                st.text(f'Timeline: {graph_element_list[i]["edge"][2]}')
+                st.text(f'Derived Nodes: \n')
+                st.text(f'  Name: {graph_element_list[i]["src"][0]}')
+                st.text(f'  Task: {graph_element_list[i]["dest"][0]}')
+                st.text(f'  Timeline: {graph_element_list[i]["edge"][2]}')
             with col2:
                 st.write(f'See something wrong?')
                 fixed_name = st.text_input('Enter the correct Name', key=sent+'fixname')
@@ -67,8 +71,10 @@ if input_file:
                     graph_element_list_fixed[i]["dest"][0],
                     graph_element_list_fixed[i]["edge"][2],
                     graph_element_list_fixed[i]["edge"][3])
+            st.text('-'*100)
 
-                
+
+           
     with initial_graph:
         knowledge_graph = Graph('meeting_knowledge_graph.html')
         for graph_elements in graph_element_list:
@@ -78,16 +84,46 @@ if input_file:
             knowledge_graph.add_edge(*graph_elements["edge"])
         knowledge_graph.display_graph()
         source_code = knowledge_graph.get_html()
-        components.html(source_code, height=1500, width=800)
+        components.html(source_code, height=800, width=800)
+
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.metric(label='Number of sentences', value=len(sent_list))
+        with col2:
+            st.metric(label='Number of nodes', value=knowledge_graph.get_node_count())
+        with col3:
+            st.metric(label='Number of edges', value=knowledge_graph.get_edge_count())
 
     
     with user_corrected_graph:
+        user_data_submission = st.button('Submit Correction')
+        if user_data_submission:
+            bar = st.progress(10)
+            # commented out for now to prevent sensitive data from being saved
+            # with open('user_corrected_data.txt', 'w') as file:
+            #     file.write(json.dumps(graph_element_list_fixed))
+            time.sleep(.5)
+            bar.progress(100)
+            st.text('Submitted! Thanks for the contribution!')
+            time.sleep(1)
+            bar.progress(0)
+            
+        
         corrected_knowledge_graph = Graph('corrected_graph.html')
+
         for graph_elements in graph_element_list_fixed:
             corrected_knowledge_graph.add_node(*graph_elements["src"])
             corrected_knowledge_graph.add_node(*graph_elements["dest"])
             corrected_knowledge_graph.add_edge(*graph_elements["edge"])
         corrected_knowledge_graph.display_graph()
         corrected_source_code = corrected_knowledge_graph.get_html()
-        components.html(corrected_source_code, height=1500, width=800)
+        components.html(corrected_source_code, height=800, width=800)
+        
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            st.metric(label='Number of sentences', value=len(sent_list))
+        with col2:
+            st.metric(label='Number of nodes', value=corrected_knowledge_graph.get_node_count())
+        with col3:
+            st.metric(label='Number of edges', value=corrected_knowledge_graph.get_edge_count())
 
--- nub_ai_meeting_tools/core.py
+++ nub_ai_meeting_tools/core.py
@@ -4,25 +4,32 @@ import networkx as nx
 class Graph:
     
     def __init__(self, html_filename, bgcolor='#22222'):
-        self.net = Network(height="1000px", width="780", bgcolor="white", font_color="black")
-        self.net.barnes_hut()
+        self.net = Network(height="780px", width="780", bgcolor="white", font_color="black")
+        self.net.barnes_hut(central_gravity=3.0, spring_strength=0.11, damping=0.1)
         self.html_filename = html_filename
+
+
     
     def add_node(self, node_id, node_label, node_type):
         if node_type == 'person':
-            self.net.add_node(node_id, label=node_label, title=node_type, shape='dot', size=50, color='#2daaf7', font_size=150)
+            self.net.add_node(node_id, label=node_label, title=node_type, shape='dot', size=50, color='#2daaf7')
         elif node_type == 'task':
-            self.net.add_node(node_id, label=node_label, title=node_type, shape='star', size=50, color='#f7c12d', font_size=150)
+            self.net.add_node(node_id, label=node_label, title=node_type, shape='star', size=50, color='#f7c12d')
         else:
-            self.net.add_node(node_id, label=node_label, title=node_type, shape='triangleDown', size=50, color='#eeedf5', font_size=150)
+            self.net.add_node(node_id, label=node_label, title=node_type, shape='triangleDown', size=50, color='#eeedf5')
     
     def add_edge(self, src_id, dest_id, label, title):
-        self.net.add_edge(src_id, dest_id, label=label, title=title, font_size=150)
+        self.net.add_edge(src_id, dest_id, label=label, title=title, width=5, font_size=150)
+
+    def get_node_count(self):
+        return len(self.net.nodes)
+    def get_edge_count(self):
+        return len(self.net.edges)
 
 
 
     def display_graph(self):
-        self.net.show_buttons(filter_=['physics'])
+        # self.net.show_buttons(filter_=['physics'])
         self.net.show(self.html_filename)
     
     def get_html(self):
